d50afbf32547be05fec48f62d79ad249adf85b4a
James Robert <jiaaro@gmail.com>
2013-05-08 11:44:44 -0400

    remove unnecessary logging

--- store/src/main.js
+++ store/src/main.js
@@ -4,12 +4,8 @@
   $(function() {
     console.log("dom ready");
 
-    var api = DEBUG && typeof adobeDPS == "undefined" ? MockAPI : adobeDPS;
+    var api = (DEBUG && typeof adobeDPS == "undefined") ? MockAPI : adobeDPS;
     
-    console.log("DEBUG: " + DEBUG);
-    console.log("adobeDPS: ", window['adobeDPS']);
-    console.log("adobeDPS.version: " + (window['adobeDPS'] || {})['version']);
-
     api.initializationComplete.addOnce(function() {
       console.log("init complete");
       new App.views.Main({api: api}).render();
