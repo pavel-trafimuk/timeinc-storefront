8ac7131df2e8d23ca74b14c016245b34759a48dd
James Robert <jiaaro@gmail.com>
2013-05-09 12:24:24 -0400

    add cache-busting to build process

--- bin/render_index.py
+++ bin/render_index.py
@@ -1,10 +1,17 @@
 import sys
+import time
 
 tmpl_file = sys.argv[1]
 out_file = sys.argv[2]
 js_files = sys.argv[3:]
 
-script_tmpl = "<script src='{0}'></script>"
+DEV_MODE = ".dev." in out_file
+
+if DEV_MODE:
+    script_tmpl = "<script src='{0}?v=" + str(int(time.time())) + "'></script>"
+else:
+    script_tmpl = "<script src='{0}'></script>"
+
 js_files = "\n".join(script_tmpl.format(src) for src in js_files)
 
 tmpl = open(tmpl_file, 'r').read()
