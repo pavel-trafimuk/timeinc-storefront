a762f9d35effdc78f69fb7fa64a2fd0964f6393d
James Robert <jiaaro@gmail.com>
2013-05-23 13:34:32 -0400

    gif compresses better than png for the image mask

--- store/src/preloader.js
+++ store/src/preloader.js
@@ -1,5 +1,4 @@
 
-new Image().src = settings.cover_spacer_img;
-new Image().src = "images/mask.png";
+new Image().src = "images/mask.gif";
 new Image().src = "images/" + settings.brandCode + "/curl.png";
-
+new Image().src = settings.cover_spacer_img;
