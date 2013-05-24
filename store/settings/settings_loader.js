d77e6a629003246b6fd6fd9c11a988d0027b4272
James Robert <jiaaro@gmail.com>
2013-05-24 14:07:57 -0400

    use cmdev in lieu of production URLS for now

--- store/settings/settings_loader.js
+++ store/settings/settings_loader.js
@@ -3,13 +3,16 @@
     // via ajax like the existing apps
     var default_settings = {
         "omniture_account": null,
-        "PRODUCTION_TCM_FEED": "invalid://fill.me.in/todo",
+        "PRODUCTION_TCM_FEED": "http://ecom-dev01-app.usdlls2.savvis.net:10400/html/v25app/data/" + settings.brandCode + ".json",
         "DEV_TCM_FEED": "http://ecom-dev01-app.usdlls2.savvis.net:10400/html/v25app/data/" + settings.brandCode + ".json",
         
         "dev_tcmfeed_image_root": "http://ecom-dev01-app.usdlls2.savvis.net:10400/html/v25app/data/images/",
-        "prod_tcmfeed_image_root": "...",
+        "prod_tcmfeed_image_root": "http://ecom-dev01-app.usdlls2.savvis.net:10400/html/v25app/data/images/",
         "adbfeed_cover_dimensions": [388, 507],
         "cover_spacer_img": "images/" + settings.brandCode + "/cover_spacer.gif",
+        
+        "dev_asset_root": "",
+        "prod_asset_root": "http://ecom-dev01-app.usdlls2.savvis.net:10400/appstorefronts-jq/"+settings.brandCode+"/adobe/apple/ipad/v25/",
 
     ihatethelastcomma: true};
 
