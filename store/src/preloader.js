0055fc6785009d898366d26185ae384e8aa95394
James Robert <jiaaro@gmail.com>
2013-05-29 11:07:24 -0400

    app wasn't launching because cb was undefined

--- store/src/preloader.js
+++ store/src/preloader.js
@@ -3,5 +3,5 @@ App.preload = function(cb) {
   new Image().src = settings.asset_root + "images/mask.gif";
   new Image().src = settings.asset_root + "images/" + settings.brandCode + "/curl.png";
   new Image().src = settings.asset_root + settings.cover_spacer_img;
-  cb();
+  (cb||$.noop)();
 }
