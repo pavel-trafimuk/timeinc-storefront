d0bb2322ec6ef3dd2d83e199b6e51f8c40387dc7
James Robert <jiaaro@gmail.com>
2013-05-31 16:06:10 -0400

    update settings to production urls

--- store/settings/settings_loader.js
+++ store/settings/settings_loader.js
@@ -3,16 +3,17 @@
     // via ajax like the existing apps
     var default_settings = {
         "omniture_account": null,
-        "PRODUCTION_TCM_FEED": "http://ecom-dev01-app.usdlls2.savvis.net:10400/html/v25app/data/" + settings.brandCode + ".json",
+
         "DEV_TCM_FEED": "http://ecom-dev01-app.usdlls2.savvis.net:10400/html/v25app/data/" + settings.brandCode + ".json",
+        "PRODUCTION_TCM_FEED": "http://subscription-assets.timeinc.com/prod/assets/appstorefronts-jq/v25data/" + settings.brandCode + ".json",
         
         "dev_tcmfeed_image_root": "http://ecom-dev01-app.usdlls2.savvis.net:10400/html/v25app/data/images/",
-        "prod_tcmfeed_image_root": "http://ecom-dev01-app.usdlls2.savvis.net:10400/html/v25app/data/images/",
+        "prod_tcmfeed_image_root": "http://subscription-assets.timeinc.com/prod/assets/appstorefronts-jq/v25data/images/",
         "adbfeed_cover_dimensions": [388, 507],
         "cover_spacer_img": "images/" + settings.brandCode + "/cover_spacer.gif",
         
-        "dev_asset_root": "",
-        "prod_asset_root": "http://ecom-dev01-app.usdlls2.savvis.net:10400/appstorefronts-jq/"+settings.brandCode+"/adobe/apple/ipad/v25/",
+        "dev_asset_root": "http://ecom-dev01-app.usdlls2.savvis.net:10400/appstorefronts-jq/"+settings.brandCode+"/adobe/apple/ipad/v25/",
+        "prod_asset_root": "http://subscription-assets.timeinc.com/prod/assets/appstorefronts-jq/"+settings.brandCode+"/adobe/apple/ipad/v25/",
 
     ihatethelastcomma: true};
 
