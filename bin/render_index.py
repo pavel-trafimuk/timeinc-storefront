c4864be13ee2f1684ca6faaf8124c37a4af0d7ba
James Robert <jiaaro@gmail.com>
2013-06-04 12:39:06 -0400

    Add cache busting to CSS

--- bin/render_index.py
+++ bin/render_index.py
@@ -9,6 +9,7 @@ js_files = sys.argv[3:]
 DEV_MODE = ".dev." in out_file
 prefix = os.environ.get("REMOTE_ASSET_PREFIX", "")
 mag_code = os.environ.get("MAG_CODE", "")
+css_hash = os.environ.get("CSS_HASH", str(int(time.time())))
 
 if DEV_MODE:
     script_tmpl = "<script src='{prefix}{0}?v=" + str(int(time.time())) + "'></script>"
@@ -22,6 +23,7 @@ tmpl = open(tmpl_file, 'r').read()
 tmpl = tmpl.replace("{{JS_FILES}}", js_files)
 tmpl = tmpl.replace("{{REMOTE_ASSET_PREFIX}}", prefix)
 tmpl = tmpl.replace("{{MAG_CODE}}", mag_code)
+tmpl = tmpl.replace("{{CSS_HASH}}", css_hash)
 
 with open(out_file, "w") as f:
   f.write(tmpl)
