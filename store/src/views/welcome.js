a77c76125be0c3b4d9901080ed92f56dd996bd56
James Robert <jiaaro@gmail.com>
2013-06-12 09:55:17 -0400

    support landscape mode on welcome screen

--- store/src/views/welcome.js
+++ store/src/views/welcome.js
@@ -13,18 +13,37 @@
           transaction;
 
       this.folio = App.api.libraryService.get_touted_issue();
+      $(window).on("resize", _.bind(this.update_covers, this));
     },
     render: function() {
-      var covers = this.folio.get_welcome_imgs();
+      var covers = this._get_covers();
       var cx = {settings:settings, img_only_cover_url: covers[0], full_cover_url: covers[1]};
       this.$el.html(this.template(cx));
       return this;
     },
+    _get_covers: function() {
+      var $window = $(window),
+          is_portrait = $window.height() > $window.width();
+      if (is_portrait) return this.folio.get_welcome_imgs().portrait;
+      else return this.folio.get_welcome_imgs().landscape;
+    },
+    update_covers: function() {
+      var covers = this._get_covers(),
+          $before = this.$(".cover-img-only"),
+          $after = this.$(".cover-with-text");
+      
+      if (covers[0]) $before.attr("src", covers[0]).show();
+      else $before.hide();
+
+      if (covers[1]) $after.attr('src', covers[1]).show();
+      else $after.hide();
+    },
     animate: function(cb) {
       cb = cb || $.noop;
       var that = this,
-          covers = this.folio.get_welcome_imgs();
-      
+          covers = this._get_covers(),
+          imgs = this.folio.get_welcome_imgs();
+
       async.parallel([
         function(cb) { 
           App.preload.img(covers[0], function() {
@@ -32,11 +51,14 @@
             cb(null);
           })
         },
-        function(cb) { App.preload.img(covers[1], _.partial(cb, null)) }
+        function(cb) { App.preload.img(imgs.portrait[0], _.partial(cb, null)) },
+        function(cb) { App.preload.img(imgs.portrait[1], _.partial(cb, null)) },
+        function(cb) { App.preload.img(imgs.landscape[0], _.partial(cb, null)) },
+        function(cb) { App.preload.img(imgs.landscape[1], _.partial(cb, null)) }
       ], function() {
-          var init_delay = covers[0] ? 1000 : 0;
-          that.$(".cover-with-text").transition({duration: 1500, delay: init_delay, opacity: 1.0});
-          that.$(".buttons").transition({duration: 600, delay: init_delay+1300, y: 0});
+          var init_delay = covers[0] ? 600 : 0;
+          that.$(".cover-with-text").transition({duration: 1800, delay: init_delay, opacity: 1.0});
+          that.$(".buttons").transition({duration: 600, delay: init_delay+1800, y: 0});
           that.$(".already-have-account").transition({duration: 1000, delay: init_delay+3100, opacity: 1.0});
         
           setTimeout(function() {
