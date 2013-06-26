fd732b0207d2d5c33b71fda19d6e06819c9714ef
James Robert <jiaaro@gmail.com>
2013-06-26 15:48:38 -0400

    add basic support for logging user actions into the DPS omnivore suite

--- store/src/app.js
+++ store/src/app.js
@@ -12,6 +12,15 @@
       reload: function() {
         window.location.reload(true);
       }
+    },
+    dps_omniture_event: function(evt_name, page_name) {
+      if (page_name !== undefined) page_name = "store|"+page_name;
+      else page_name = TcmOmni.get_pagename();
+
+      App.api.analyticsService.trackCustomEvent("customEvent3", {
+        customVariable3: evt_name,
+        customVariable4: page_name
+      });
     }
   }
   _.extend(App, Backbone.Events);
