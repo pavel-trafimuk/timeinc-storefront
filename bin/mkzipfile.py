0986850621481bedaca8453941372b79818c9925
James Robert <jiaaro@gmail.com>
2013-07-22 10:05:15 -0400

    use zlib's deflate compression

--- bin/mkzipfile.py
+++ bin/mkzipfile.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from os.path import join, dirname
 import sys
-from zipfile import ZipFile
+from zipfile import ZipFile, ZIP_DEFLATED
 
 link_destination = sys.argv[1]
 out_file = sys.argv[2]
@@ -11,6 +11,6 @@ tmpl_path = join(dirname(__file__), "embed_index.html.tmpl")
 tmpl = open(tmpl_path, 'r').read()
 tmpl = tmpl.replace("{{URL}}", link_destination)
 
-z = ZipFile(out_file, "w")
+z = ZipFile(out_file, "w", ZIP_DEFLATED)
 z.writestr("index.html", tmpl)
 z.close()
