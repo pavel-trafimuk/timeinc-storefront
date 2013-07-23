d6aef7cc328bcd99d342e3e7c1767a5e999f981b
Tim Krofecheck <tim_krofecheck@timeinc.com>
2013-07-23 11:05:29 -0400

    files modified for InStyle build

---
+++ store/settings/IN.js
@@ -0,0 +1,44 @@
+(function() {
+// One of the big benefits of using a js file instead of json for settings
+// is the ability to add comments
+window.settings = {
+
+    // General settings
+    "brandName"                  : "INSTYLE",
+    "brandCode"                  : "IN",
+    "schedule"                   : "weekly",
+    "popupInterval"              : 0,
+    "enable_first_load_popup"    : false,
+    "subscribeOfferText"         : "Subscribe now to get INSTYLE Magazine on your iPad. Featuring the hottest fashion trends, beauty tips, celebrity style secrets, best beauty buys and so much more! Plus, get 1 month free with an annual subscription.",
+    
+    "welcome_preview": "adobe",
+    "hero_preview": "adobe",
+    "hero_itii_preview": "adobe",
+    "max_back_issues"            : 20,
+
+    "omniture_account"           : "timaginstylenk",
+    "omniture_server"            : "timeinc.122.2o7.net",
+    "omniture_ssl_server"        : "timeinc.122.2o7.net",
+
+    // http://mageng.it.timeinc.com/twiki/bin/view/Main/DPS-MagazineXML-Urls
+    "adobeAppId"                 : "8d7346ec5a504f8d97dea533be3596e1",
+    "appId"                      : "com.timeinc.instyle.ipad.inapp",
+    
+    "supportPhoneNumber"         : "1-866-772-8793",
+    "supportEmail"               : "digital@instyle.customersvc.com",
+
+    // ************************************************************  
+    // App specific URLS
+    // ************************************************************  
+    "lucieRegistrationURL"       : "https://subscription.timeinc.com/storefront/site/in-lucie-customer-creation-itunes201307.html",
+    "welcomeScreenWesURL"        : "https://secure.customersvc.com/servlet/Show?WESPAGE=am/tablet/template/login.jsp&MSRSMAG=IN&MSDDMOFF=ABTF&MSDTRACK=IPSP&MSDVNDID=TBLT",
+    "WesPageURL"                 : "https://secure.customersvc.com/servlet/Show?WESPAGE=am/tablet/template/login.jsp&MSRSMAG=IN&MSDDMOFF=ABTF&MSDTRACK=IPAD&MSDVNDID=TBLT",
+    "licenceAgreementUrl"        : "http://www.timeinc.net/subs/privacy/eula/in/agreement.html",
+    "privacyPolicyUrl"           : "https://subscription.timeinc.com/storefront/privacy/instyle/generic_privacy_new.html?dnp-source=H",
+    "termsOfUseUrl"              : "https://subscription.timeinc.com/storefront/privacy/instyle/privacy_terms_service.html",
+
+    "adobeFeedUrl"               : "http://subscription-assets.timeinc.com/prod/assets/appstorefronts-jq/adobe-feeds/com.timeinc.instyle.ipad.inapp.xml",
+    "adobeFeedUrl_dev"           : "http://ecom-dev01-app.usdlls2.savvis.net:10500/assets/appstorefronts-jq/adobe-feeds/com.timeinc.instyle.ipad.inapp.xml",
+    
+"ihatethelastcomma": true};
+})();
