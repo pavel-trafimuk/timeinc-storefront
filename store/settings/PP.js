3852104408d761a7c71e02ebec49b13c35dd480f
Tim Krofecheck <tim_krofecheck@timeinc.com>
2013-07-23 11:05:50 -0400

    files modified for People en Espanol build

---
+++ store/settings/PP.js
@@ -0,0 +1,44 @@
+(function() {
+// One of the big benefits of using a js file instead of json for settings
+// is the ability to add comments
+window.settings = {
+
+    // General settings
+    "brandName"                  : "PEOPLE EN ESPA&Ntilde;OL",
+    "brandCode"                  : "PP",
+    "schedule"                   : "monthly",
+    "popupInterval"              : 0,
+    "enable_first_load_popup"    : false,
+    "subscribeOfferText"         : "&iexcl;Suscr&iacute;bete ahora para ver deslumbrantes fotos y v&iacute;deos exclusivos detr&aacute;s de c&aacute;mara. Adem&aacute;s, entrevistas francas con tus celebridades favoritas!",
+    
+    "welcome_preview": "adobe",
+    "hero_preview": "adobe",
+    "hero_itii_preview": "adobe",
+    "max_back_issues"            : 20,
+
+    "omniture_account"           : "timagallyounk",
+    "omniture_server"            : "timeinc.122.2o7.net",
+    "omniture_ssl_server"        : "timeinc.122.2o7.net",
+
+    // http://mageng.it.timeinc.com/twiki/bin/view/Main/DPS-MagazineXML-Urls
+    "adobeAppId"                 : "13617fe46d974ecfa5639a33c4e28667",
+    "appId"                      : "com.timeinc.peopleespanol.ipad.inapp",
+    
+    "supportPhoneNumber"         : "1-866-772-8730",
+    "supportEmail"               : "digital@espanol.customersvc.com",
+
+    // ************************************************************  
+    // App specific URLS
+    // ************************************************************  
+    "lucieRegistrationURL"       : "https://subscription.timeinc.com/storefront/site/pp-lucie-customer-creation-itunes201307.html",
+    "welcomeScreenWesURL"        : "https://secure.customersvc.com/servlet/Show?WESPAGE=am/tablet/template/login.jsp&MSRSMAG=PP&MSDDMOFF=ABTF&MSDTRACK=IPSP&MSDVNDID=TBLT",
+    "WesPageURL"                 : "https://secure.customersvc.com/servlet/Show?WESPAGE=am/tablet/template/login.jsp&MSRSMAG=PP&MSDDMOFF=ABTF&MSDTRACK=IPAD&MSDVNDID=TBLT",
+    "licenceAgreementUrl"        : "http://www.timeinc.net/subs/privacy/eula/pp/agreement.html",
+    "privacyPolicyUrl"           : "https://subscription.timeinc.com/storefront/privacy/pespanol/generic_privacy_new.html?dnp-source=I",
+    "termsOfUseUrl"              : "https://subscription.timeinc.com/storefront/privacy/pespanol/privacy_terms_service.html",
+
+    "adobeFeedUrl"               : "http://subscription-assets.timeinc.com/prod/assets/appstorefronts-jq/adobe-feeds/com.timeinc.allyou.ipad.inapp.xml",
+    "adobeFeedUrl_dev"           : "http://ecom-dev01-app.usdlls2.savvis.net:10500/assets/appstorefronts-jq/adobe-feeds/com.timeinc.allyou.ipad.inapp.xml",
+    
+"ihatethelastcomma": true};
+})();
