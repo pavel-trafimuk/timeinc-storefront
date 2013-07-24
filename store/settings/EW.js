cb638428790fca4281b7adce9ea6d2a383e8c2be
Tim Krofecheck <tim_krofecheck@timeinc.com>
2013-07-24 16:34:44 -0400

    added echo settings and externalized first load popup template

--- store/settings/EW.js
+++ store/settings/EW.js
@@ -9,6 +9,13 @@ window.settings = {
     "schedule"                   : "weekly",
     "popupInterval"              : 5,
     "enable_first_load_popup"    : true,
+    
+    "popup_paragraph1"           : "<b>We heard you!</b><br>The Result: <b><i>Best Entertainment Weekly ever!</i></b>",
+    "popup_paragraph2"           : "You wanted support for Retina display &mdash; <b>we've got it.</b><br>You wanted Pinch &amp; Zoom &mdash; <b>we have it.</b><br>You wanted a better App &mdash; <b>we've done it.</b>",
+    "popup_restore_text"         : "<i>To restore your back issues follow instructions in the Library</i>",
+    "popup_subscribe_text"       : "Download the latest issue now",
+    "popup_close_text"           : "Close",
+    
     "subscribeOfferText"         : "Be the first to know about the best (and worst) in movies, TV, music, books, and more. PLUS: You can watch trailers, sample songs, as well as buy movie tickets, books, and music instantly—without ever leaving your couch.",
     
     "welcome_preview": "adobe",
@@ -19,6 +26,9 @@ window.settings = {
     "omniture_account"           : "timagewmagnk",
     "omniture_server"            : "timeinc.122.2o7.net",
     "omniture_ssl_server"        : "timeinc.122.2o7.net",
+    
+    "echo_bundle_id"             : "com_timeinc_ew_ipad_inapp",
+    "echo_token"                 : "749e1973e9b6f1b940ddf0465da96a22f578a35f",
 
     // http://mageng.it.timeinc.com/twiki/bin/view/Main/DPS-MagazineXML-Urls
     "adobeAppId"                 : "72c6d01e1eb34f07bf2d2c31643f0646",
