ba24d2815294d4869d30ef1a36003879a6679c21
James Robert <jiaaro@gmail.com>
2013-07-24 15:15:13 -0400

    bug fixes for emags

--- store/src/main.js
+++ store/src/main.js
@@ -41,27 +41,34 @@ $(function() {
       });
 
       // eMags tracking for Subscriptions and SingleCopy sales. "FreeSample" not yet implemented
-      App.api.receiptService.newReceiptsAvailableSignal.add(function(receipt) {
-        var evt_type, product_id, price, sub, folio;
+      App.api.receiptService.newReceiptsAvailableSignal.add(function(receipts) {
+        var receipt, evt_type, product_id, price, sub, folio;
 
+        console.log("newReceiptsAvailableSignal fired", receipts);
         if (!settings.eMagsAppId) return;
 
-        product_id = receipt.productId;
-        if (receipt.isSubscription) {
-          sub = App.api.receiptService.availableSubscriptions[product_id];
-          price = sub.price;
-          evt_type = (sub.duration.toLowerCase() == "1 month") ? "MonthlySubscription" : "AnnualSubscription";
-        }
-        else {
-          folio = App.api.libraryService.get_by_productId(product_id);
-          price = folio.price;
-          evt_type = "SingleCopySale";
-        }
-        
-        // remove dollar signs and other currecy symbols
-        price = price.replace(/[^\d\.]/gi, "");
+        console.log("tracking " + receipts.length + " new receipts");
+        for (var i=receipts.length; i--;) {
+          receipt = receipts[i];
 
-        EMPurchase(evt_type, product_id, price);
+          product_id = receipt.productId;
+          if (receipt.isSubscription) {
+            sub = App.api.receiptService.availableSubscriptions[product_id];
+            price = sub.price;
+            evt_type = (sub.duration.toLowerCase() == "1 month") ? "MonthlySubscription" : "AnnualSubscription";
+          }
+          else {
+            folio = App.api.libraryService.get_by_productId(product_id);
+            price = folio.price;
+            evt_type = "SingleCopySale";
+          }
+          
+          // remove dollar signs and other currecy symbols
+          price = price.replace(/[^\d\.]/gi, "");
+
+          console.log("tracking eMags Purchase", evt_type, product_id, price);
+          EMPurchase(evt_type, product_id, price);
+        }
       });
     });
   });
