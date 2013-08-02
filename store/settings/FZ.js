e3f10e9ef9fdda949b064a4ab34ef734e4e32847
James Robert <jiaaro@gmail.com>
2013-08-02 17:13:21 -0400

    update a good portion of the international app's settings

--- store/settings/FZ.js
+++ store/settings/FZ.js
@@ -8,25 +8,25 @@ window.settings = {
     "brandCode"                  : "FZ",
     "schedule"                   : "weekly",
     "popupInterval"              : 5,
-    "enable_first_load_popup"    : true,
+    "enable_first_load_popup"    : false,
  
-    "subscribeOfferText"         : "Subscribe now to PEOPLE Magazine on your iPad. The Digital Edition gets you extras like bonus photos, exclusive videos, movie trailers, song samples, story updates from PEOPLE.com, click-to-buy features and so much more! Plus, get 1 month free with a semi-annual subscription.",
+    "subscribeOfferText"         : "",
     
     "welcome_preview": "image",
     "hero_preview": "image",
     "hero_itii_preview": "image",
     "max_back_issues"            : 20,
 
-    "omniture_account"           : "timagpeoplenk",
-    "omniture_server"            : "metrics.people.com",
-    "omniture_ssl_server"        : "smetrics.people.com",
+    "omniture_account"           : "",
+    "omniture_server"            : "",
+    "omniture_ssl_server"        : "",
 
-    "echo_bundle_id"             : "com_timeinc_people_ipad_inapp",
-    "echo_token"                 : "aa42b59482e5836321241326aa2d5320192089e1",
+    "echo_bundle_id"             : "",
+    "echo_token"                 : "",
     
     // http://mageng.it.timeinc.com/twiki/bin/view/Main/DPS-MagazineXML-Urls
-    "adobeAppId"                 : "8632a117e7124ea988555c2dd41aa858",
-    "appId"                      : "com.timeinc.people.ipad.inapp",
+    "adobeAppId"                 : "5e020d776f0d4ccda413ffa1ad685e89",
+    "appId"                      : "com.international.fortune.ipad.inapp",
 
     "supportPhoneNumber"         : "1-866-769-0199",
     "supportEmail"               : "peopledigital@customersvc.com",
@@ -34,10 +34,10 @@ window.settings = {
     // ************************************************************  
     // App specific URLS
     // ************************************************************  
-    "lucieRegistrationURL"       : "https://subscription.timeinc.com/storefront/site/pe-lucie-customer-creation-itunes201307.html",
-    "welcomeScreenWesURL"        : "https://secure.customersvc.com/servlet/Show?WESPAGE=am/tablet/template/login.jsp&MSRSMAG=PE&MSDDMOFF=ABTF&MSDTRACK=IPSP&MSDVNDID=TBLT",
-    "WesPageURL"                 : "https://secure.customersvc.com/servlet/Show?WESPAGE=am/tablet/template/login.jsp&MSRSMAG=PE&MSDDMOFF=ABTF&MSDTRACK=IPAD&MSDVNDID=TBLT",
-    "licenceAgreementUrl"        : "http://www.timeinc.net/subs/privacy/eula/pe/agreement.html",
+    "lucieRegistrationURL"       : "https://subscription.timeinc.com/storefront/site/fz-lucie-customer-creation-itunes201307.html",
+    "welcomeScreenWesURL"        : "https://secure.customersvc.com/servlet/Show?WESPAGE=am/tablet/template/login.jsp&MSRSMAG=FZ&MSDDMOFF=ABTF&MSDTRACK=IPSP&MSDVNDID=TBLT",
+    "WesPageURL"                 : "https://secure.customersvc.com/servlet/Show?WESPAGE=am/tablet/template/login.jsp&MSRSMAG=FZ&MSDDMOFF=ABTF&MSDTRACK=IPAD&MSDVNDID=TBLT",
+    "licenceAgreementUrl"        : "http://www.timeinc.net/subs/privacy/eula/fz/agreement.html",
     "privacyPolicyUrl"           : "https://subscription.timeinc.com/storefront/privacy/people/generic_privacy_new.html?dnp-source=H",
     "termsOfUseUrl"              : "https://subscription.timeinc.com/storefront/privacy/people/privacy_terms_service.html",
 
@@ -46,4 +46,5 @@ window.settings = {
     
 "ihatethelastcomma": true};
 })();
+
 
