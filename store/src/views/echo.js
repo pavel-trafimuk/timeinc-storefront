da814d283dde51a0df5835bb191b010ff6c396ac
Tim Krofecheck <tim_krofecheck@timeinc.com>
2013-08-21 12:43:36 -0400

    allow post to echo after store loads (user signs in)

--- store/src/views/echo.js
+++ store/src/views/echo.js
@@ -9,31 +9,76 @@
     },
     render: function() {
       var that = this,
-          cx,
-          echo_url = settings.echo_stage,
-          d_connected = App.api.deviceService.isOnline,
-          d_email = App.api.authenticationService.userName,
+          apiLoad, d_connected, d_email, d_omni_visitor_id, d_push_token, cx;
+
+      //defaults
+      this.postMade = false;
+      this.echo_url = settings.echo_stage;
+      
+      apiLoad = window.setInterval(loadEcho(), 2000);
+      
+      function loadEcho() {
+        window.setInterval(function() {
+          window.clearInterval(apiLoad);
+          d_authenticated = adobeDPS.authenticationService.isUserAuthenticated,
           d_omni_visitor_id = App.api.deviceService.omnitureVisitorId.toString().replace(/-/g,""),
           d_push_token = App.api.deviceService.pushNotificationToken.toString();
-          
-      if (settings.echoENV == "prod") { echo_url = settings.echo_prod; }
-      
-      cx = {
-          settings: settings,
-          data : { echo_url: echo_url, d_connected: d_connected, d_email: d_email, d_omni_visitor_id: d_omni_visitor_id, d_push_token: d_push_token }
-      };
-      
-      this.$el.html(this.template(cx)).appendTo("body");
-      //alert(this.template(cx).toString());
-      
-      var postEchoInterval = window.setInterval(function() {
-          if ($("#echoForm") && $("#echoFrame")) {
-              window.clearInterval(postEchoInterval);
-              setTimeout(function() { $("#echoForm").submit() }, 1000);
-              console.log("Echo: " + $("#echoFrame").contents().find("body").html());
+            
+          if (settings.echoENV == "prod") {
+            that.echo_url = settings.echo_prod;
           }
-      }, 0);
+            
+          //Is user logged in?
+          if (d_authenticated) {
+            // User logged in : Get email address from API
+            d_email = App.api.authenticationService.userName;
+              
+            if (d_email && (!localStorage.echoEmail || localStorage.echoEmail != d_email)) {
+              // Email address is different then last time, or this is the first visit, echo record should be updated
+              localStorage.echoEmail = d_email;
+              //alert(localStorage.echoEmail);
+              that.postMade = false;
+            }
+          } else {
+            //User not logged in : Get email address from localStorage (if available)
+            if (localStorage.echoEmail) {
+              d_email = localStorage.echoEmail;
+            }
+          }
+            
+          //Post has not been made
+          if (!that.postMade) {
+            // Remove form (if exists)
+            if (that.$el.find("#echoForm")) {
+              that.$el.find("#echoForm").remove();
+            }
+              
+            // Remove frame (if exists)
+            if (that.$el.find("#echoFrame")) {
+              that.$el.find("#echoFrame").remove();
+            }
+              
+            // Update cx object
+            cx = {
+              settings: settings,
+              data : { echo_url: that.echo_url, d_email: d_email, d_omni_visitor_id: d_omni_visitor_id, d_push_token: d_push_token }
+            };
+            //alert(JSON.stringify(cx));
+            
+            // Redraw form and iframe using cx object to populate form input values
+            that.$el.html(that.template(cx)).appendTo("body");
+            //alert(that.template(cx));
+          }
+              
+            
+          d_connected = App.api.deviceService.isOnline;
+          //Continue if connected
+          if (d_connected && d_push_token && d_omni_visitor_id && !that.postMade) {
+            $("#echoForm").submit();
+            that.postMade = true;
+          }
+        }, 2000);
+      }
     }
   });
-  
 })();
