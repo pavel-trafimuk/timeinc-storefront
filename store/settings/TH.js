af641d779d41229cc064d1b776d8d1a9dfeca08a
James Robert <jiaaro@gmail.com>
2013-09-23 16:41:41 -0400

    No welcome screen in TH

--- store/settings/TH.js
+++ store/settings/TH.js
@@ -7,7 +7,7 @@ window.settings = {
     "brandName"                  : "This Old House",
     "brandCode"                  : "TH",
     "schedule"                   : "monthly",
-    "popupInterval"              : 5,
+    "popupInterval"              : 0,
     "enable_first_load_popup"    : true,
     
     "subscribeOfferText"         : "Subscribe to THIS OLD HOUSE and get tips and advice from the experts on how to enhance & beautify your home in big and small ways!",
