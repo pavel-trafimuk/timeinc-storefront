4561ae64dcf04f0133a50e016b9608e8a5113990
James Robert <jiaaro@gmail.com>
2013-10-01 15:05:48 -0400

    PE performs really badly, so let's try reducing the number of issues (and by extension, DOM elements)

--- store/settings/PE.js
+++ store/settings/PE.js
@@ -21,7 +21,7 @@ window.settings = {
     "welcome_preview": "image",
     "hero_preview": "image",
     "hero_itii_preview": "image",
-    "max_back_issues"            : 20,
+    "max_back_issues"            : 12,
 
     "storeBuyIssueBtn"           : "Buy Now",
     "heroHeading"                : "In This Issue",
