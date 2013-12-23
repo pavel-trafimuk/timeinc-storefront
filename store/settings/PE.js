f1b9c943da3647496ff85453ce0d9b56715e8540
James Robert <jiaaro@gmail.com>
2013-12-23 10:31:36 -0500

    change store banners
    RMS #118667

--- store/settings/PE.js
+++ store/settings/PE.js
@@ -16,9 +16,9 @@ window.settings = {
 
 
     "store_show_banners": true,
-    "store_banners_type": "buynow", // buynow OR deeplink OR subscribe
-    "store_banner_productId": "com.timeinc.people.ipad.inapp.12012013",
-    "store_banner_dossierId": "289468",
+    "store_banners_type": "subscribe", // buynow OR deeplink OR subscribe
+    //"store_banner_productId": "com.timeinc.people.ipad.inapp.12012013",
+    //"store_banner_dossierId": "289468",
     
     "storeIssuesBuyBtns": true,
 
