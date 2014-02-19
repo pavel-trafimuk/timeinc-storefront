9920973cf290a0cf743a4ba73f1c6aae93973ffa
Tim Krofecheck <tim_krofecheck@timeinc.com>
2014-02-19 14:36:43 -0500

    update folio confirmation dialog - needs works

---
+++ store/src/dialogs/update-folio.js
@@ -0,0 +1,41 @@
+
+App.dialogs.UpdateFolio = Backbone.View.extend({
+  className: "modal-background",
+  template: Handlebars.templates['dialog-updatefolio.tmpl'],
+  events: {
+    "click #cancel": "onNotNow",
+    "click #ok": "onYes"
+  },
+  initialize: function(folio) {
+    console.log("App.dialogs.UpdateFolio.initialize()");
+    this.folio = folio;
+  },
+  render: function() {
+    console.log("App.dialogs.UpdateFolio.render()");
+    
+    var cx = { folio: folio };
+    
+    this.$el.html(this.template(cx));
+    
+    return this;
+  },
+  open: function() {
+    console.log("App.dialogs.UpdateFolio.open()");
+    this.$("#updatefolio-dialog").addClass("pop");
+
+    //this.omni_pv = App.omni.pageview("subscribe", "event1");
+  },
+  onYes: function() {
+    //App.omni.event("error_cancel");
+    this.remove();
+  },
+  onNotNow: function() {
+    //App.omni.event("error_cancel");
+    folio.isUpdatable = false; //force folio to be non-updatable in order to view old sample (there has to be a better way)
+    this.remove();
+  },
+  remove: function() {
+    //TcmOmni.set_pagename(this.omni_pv.prev);
+    Backbone.View.prototype.remove.apply(this, arguments);
+  }
+});
