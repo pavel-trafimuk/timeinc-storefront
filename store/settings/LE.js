32744c784bb5a9ca27dae243cdfa5289e276b4f4
James Robert <jiaaro@gmail.com>
2014-04-14 11:28:39 -0400

    Add all necessary files for F&W and T&L

---
+++ store/settings/LE.js
@@ -0,0 +1,34 @@
+(function() {
+// One of the big benefits of using a js file instead of json for settings
+// is the ability to add comments
+window.settings = {
+
+    // General settings
+    "brandName"                  : "TRAVEL & LEISURE",
+    "brandCode"                  : "LE",
+    "schedule"                   : "monthly",
+    "popupInterval"              : 0,
+    "enable_first_load_popup"    : false,
+    
+    "heroHeading"                : "Inside This Issue",
+    
+    "hero_preview": "none",
+    "enableHeroCoverTap"         : false,
+    "preview_issue_product_id"   : "TLFreePreview_iPad",
+    
+    "max_back_issues"            : 20,
+   
+    // http://mageng.it.timeinc.com/twiki/bin/view/Main/DPS-MagazineXML-Urls
+    "adobeAppId"                 : "073ce43d9b754f2aa77bcdd6f2dff3a0",
+    "appId"                      : "com.aexp.ereader.kiosk.tl",
+
+    "supportPhoneNumber"         : "1-866-772-8789",
+    "supportEmail"               : "digital@allyou.customersvc.com",
+
+    // ************************************************************  
+    // App specific URLS
+    // ************************************************************  
+    "lucieRegistrationURL"       : "https://subscription.timeinc.com/storefront/site/ay-lucie-customer-creation-itunes201307.html",
+    
+"ihatethelastcomma": true};
+})();
---
+++ store/settings/WI.js
@@ -0,0 +1,34 @@
+(function() {
+// One of the big benefits of using a js file instead of json for settings
+// is the ability to add comments
+window.settings = {
+
+    // General settings
+    "brandName"                  : "FOOD & WINE",
+    "brandCode"                  : "WI",
+    "schedule"                   : "monthly",
+    "popupInterval"              : 0,
+    "enable_first_load_popup"    : false,
+    
+    "heroHeading"                : "Inside This Issue",
+    
+    "hero_preview": "none",
+    "enableHeroCoverTap"         : false,
+    "preview_issue_product_id"   : "FREEFWIOSPREVIEW",
+    
+    "max_back_issues"            : 20,
+
+    // http://mageng.it.timeinc.com/twiki/bin/view/Main/DPS-MagazineXML-Urls
+    "adobeAppId"                 : "c2d8715688a340bfabafa01d90e6ea98",
+    "appId"                      : "com.aexp.ereader.kiosk.fw",
+
+    "supportPhoneNumber"         : "1-866-772-8789",
+    "supportEmail"               : "digital@allyou.customersvc.com",
+
+    // ************************************************************  
+    // App specific URLS
+    // ************************************************************  
+    "lucieRegistrationURL"       : "https://subscription.timeinc.com/storefront/site/ay-lucie-customer-creation-itunes201307.html",
+    
+"ihatethelastcomma": true};
+})();
