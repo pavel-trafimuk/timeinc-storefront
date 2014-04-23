73e1d0d9b21f9206879140cbdcd2e8ed9b0eb7d5
James Robert <jiaaro@gmail.com>
2014-04-23 15:22:42 -0400

    just about done with the moretab

--- store/banners/WI/tabs/moretab/moretab.js
+++ store/banners/WI/tabs/moretab/moretab.js
@@ -1,7 +1,21 @@
+/* global $, Backbone, libBanner, Product, ProductList, HeroView, ProductView */
 (function() {
   libBanner.track_page_view("MoreTab");
   libBanner.echo();
+  
+  // Check if the user is a subscriber ASAP
+  //   when cached data is avilable, use that while checking for fresh data
+  window.sub_status = JSON.parse(localStorage.isSubscriberData || "{}");
+  libBanner.get_subscription_status(function(status) {
+    window.sub_status = status;
+    localStorage.isSubscriberData = JSON.stringify(status);
+    Backbone.trigger("subscriptionStatusUpdated", status);
 
+    var $fc = $(".free-content");
+    $fc[status.is_sub ? "addClass" : "removeClass"]("disabled");
+  });
+
+  // Setup the store
   $.getJSON("content.json", function(data) {
     var feature = new Product(data.featuredProduct[0]),
         products = new ProductList(data.digitalProducts),
@@ -28,5 +42,26 @@
     });
 
   });
+
+  // Disable Scrolling
+  var xStart, yStart = 0;
+  $(document)
+    .on("touchmove", function() {
+      return false;
+    })
+    .on("touchstart", ".cover-block, .dod-images", function(evt) {
+      var touch = evt.originalEvent.touches[0];
+      xStart = touch.screenX;
+      yStart = touch.screenY;
+    })
+    .on("touchmove", ".cover-block, .dod-images", function(evt) {
+      var touch = evt.originalEvent.touches[0],
+          xMovement = Math.abs(touch.screenX - xStart),
+          yMovement = Math.abs(touch.screenY - yStart);
+      
+      if((yMovement * 2) < xMovement) {
+        evt.stopPropagation();
+      }
+    });
 
 })();
--- store/banners/WI/tabs/moretab/views/product.js
+++ store/banners/WI/tabs/moretab/views/product.js
@@ -1,16 +1,35 @@
+/* global $, _, Backbone, sub_status */
+var Product, ProductList, HeroView, ProductView,
+	DetailOverlayDialog, SubscribeDialog,
+	libBanner;
 
-Product = Backbone.Model.extend({});
+Product = Backbone.Model.extend({
+	is_ebook: function() {
+		return !!this.get("link");
+	}
+});
 
 ProductList = Backbone.Collection.extend({
 	model: Product
 });
 
 HeroView = Backbone.View.extend({
+	events: {
+		
+	},
 	template: _.template($("#hero-template").html()),
 	render: function() {
-		var rendered = this.template(this.model.attributes);
-		this.$el.html(rendered);
+		var cx = _.clone(this.model.attributes);
+		cx.buylink = this.model.get("link");
+		cx.is_subscriber = sub_status.is_sub;
+
+		this.$el.html(this.template(cx));
 		return this;
+	},
+	show_detail: function() {
+		new DetailOverlayDialog({
+			model: this.model
+		});
 	}
 });
 ProductView = Backbone.View.extend({
@@ -27,38 +46,114 @@ ProductView = Backbone.View.extend({
 	open_detail_dialog: function() {
 		new DetailOverlayDialog({
 			model: this.model
-		})
+		});
 	}
 });
 DetailOverlayDialog = Backbone.View.extend({
 	className: "dialog",
 	template: _.template($("#overlay-template").html()),
 	events: {
+		"click .dod-button.appstore": "open_link",
+		"click .dod-button.subscribe": "subscribe",
+		"click .dod-button.download": "download",
 		"click .dod-close-button": "close"
+	},
+	initialize: function() {
+		var that = this;
+		this.render().$el.appendTo("body");
+		this.listenTo(Backbone, "subscriptionStatusUpdated", function() {
+			that.render();
+		});
 	},
 	render: function() {
 		var cx = _.clone(this.model.attributes),
 			product_id = this.model.get("productID"),
 			link = this.model.get("link");
 		
-		if (product_id) {
+		cx.buttonHref = "#";
+		
+		if (product_id && !sub_status.is_sub) {
 			cx.buttonText = "Subscribe";
 			cx.buttonClass = "subscribe";
 		}
-		else if (link) {
+		else if (product_id) {
+			cx.buttonText = "Download";
+			cx.buttonClass = "download";
+
+			try {
+				var folio = libBanner.api.libraryService.folioMap.getByProductId(product_id);
+				if (folio.isViewable) {
+					cx.buttonText = "View";
+					cx.buttonClass = "view";
+				}
+			} 
+			catch (err) {}
+		}
+		else if (this.model.is_ebook()) {
 			cx.buttonText = "Open In App Store";
 			cx.buttonClass = "appstore";
+			cx.buttonHref = link;
 		}
 		
 		this.$el.html(this.template(cx));
 		return this;
 	},
-	initialize: function() {
-		this.render().$el.appendTo("body");
+	open_link: function() {
+		// var link = this.model.get("link");
+		// link = encodeURIComponent(link);
+		// location.href = link;
+	},
+	subscribe: function(evt) {
+		evt.preventDefault();
+		new SubscribeDialog();
+	},
+	download: function() {
+		libBanner.buy_issue(this.model.get("productID"));
 	},
 	close: function() {
 		// calling remove from here to allow for
 		// a closing animation in the future
 		this.remove();
 	}
+});
+
+SubscribeDialog = Backbone.View.extend({
+    className: "dialog",
+    template: _.template($("#subscribe-dialog-template").html()),
+    events: {
+        "click .sd-close-button": "close",
+        "click .sd-subscribe-button": "onSubscribe"
+    },
+    initialize: function() {
+        this.render().$el.appendTo("body");
+	},
+	render: function() {
+		var cx = {
+			subscriptions: _(libBanner.api.receiptService.availableSubscriptions).values()
+		}
+		this.$el.html(this.template(cx));
+		return this;
+	},
+  
+    close: function() {
+        this.remove();
+    },
+    onSubscribe: function(evt) {
+        evt.preventDefault();
+    
+        var productId = $(evt.currentTarget).attr("id");
+        var subscription = libBanner.api.receiptService.availableSubscriptions[productId];
+        var transaction = subscription.purchase();
+    
+        transaction.completedSignal.addOnce(function(transaction) {
+            var success = (transaction.state == libBanner.api.transactionManager.transactionStates.FINISHED);
+            if (success) {
+                sub_status.is_sub = true;
+                sub_status.sub_type = "itunes";
+                Backbone.trigger("subscriptionStatusUpdated", sub_status);
+            }
+        });
+    
+        this.remove();
+    }
 });
