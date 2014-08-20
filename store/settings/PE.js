597ff3677c51429f8afbfe011dcfbaf58623fd26
Tim Krofecheck <tim_krofecheck@timeinc.com>
2014-08-20 09:05:08 -0400

    PE - turn AB test on for buy-issue upwell

--- store/settings/PE.js
+++ store/settings/PE.js
@@ -61,7 +61,10 @@ window.settings = {
     "storeIssuesStartingFilter"  : "!special",
     "storeIssuesTapToView"       : "<button class='bi-filter active' data-filter='!special'>Weekly Issues</button> <button class='bi-filter' data-filter='special'>Special Issues</button>",
 
-    "heroBuyIssue_useDialog"     : false,
+    "heroBuyIssue_useDialog"     : AB("heroBuyIssuePopupEnabled", {
+                                        "yes": true,
+                                        "no": false
+                                    }),
                                     
     "buyIssueSubscribeDiscount"  : "57%",
     "buyIssueTitle"              : "Get This Issue Free!",
